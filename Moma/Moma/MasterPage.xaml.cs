0e45384a919a587d5e7a3cb9538644fbefc01746
justin-yip <justin.n.yip@gmail.com>
2016-01-30 15:28:03 -0500

    #JustinYip. Pushed files that didn't get pushed by Visual Studio

---
+++ Moma/Moma/MasterPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace Moma
+{
+    public partial class MasterPage : ContentPage
+    {
+        public ListView ListView { get { return listView; } }
+
+        public MasterPage()
+        {
+            InitializeComponent();
+
+            var masterPageItems = new List<MasterPageItem>();
+            masterPageItems.Add(new MasterPageItem
+            {
+                Title = "Contact",
+                IconSource = "contacts.png",
+                TargetType = typeof(ContactPage)
+            });
+            masterPageItems.Add(new MasterPageItem
+            {
+                Title = "Settings",
+                IconSource = "todo.png",
+                TargetType = typeof(SettingsPage)
+            });
+
+            listView.ItemsSource = masterPageItems;
+        }
+    }
+}
