cbd1a05f40fbcb90d272051f5537e74d28244da1
justin-yip <justin.n.yip@gmail.com>
2016-02-07 02:26:11 -0500

    #JustinYip. Highlight selected floor and modified floor selector css. Changed marker popups.

--- Moma/Moma.Droid/Assets/Content/markers.js
+++ Moma/Moma.Droid/Assets/Content/markers.js
@@ -10,27 +10,27 @@
 });
 
 //floor1 markers (POIs)
-var floor1Marker1 = L.marker([0, 0], { icon: markerIcon }).bindPopup('0,0'),
-    floor1Marker2 = L.marker([0, 188], { icon: markerIcon }).bindPopup('0,188'),
-    floor1Marker3 = L.marker([-93, 0], { icon: markerIcon }).bindPopup('-93,0'),
-    floor1Marker4 = L.marker([-93, 188], { icon: markerIcon }).bindPopup('-93,188');
+var floor1Marker1 = L.marker([0, 0], { icon: markerIcon }).bindPopup('Arctifat 1'),
+    floor1Marker2 = L.marker([0, 188], { icon: markerIcon }).bindPopup('Arctifat 2'),
+    floor1Marker3 = L.marker([-93, 0], { icon: markerIcon }).bindPopup('Arctifat 3'),
+    floor1Marker4 = L.marker([-93, 188], { icon: markerIcon }).bindPopup('Arctifat 4');
 
 //floor2 markers
-var floor2Marker1 = L.marker([-10, 20], { icon: markerIcon }).bindPopup('0,0'),
-    floor2Marker2 = L.marker([-70, 120], { icon: markerIcon }).bindPopup('0,0');
+var floor2Marker1 = L.marker([-10, 20], { icon: markerIcon }).bindPopup('Arctifat 5'),
+    floor2Marker2 = L.marker([-70, 120], { icon: markerIcon }).bindPopup('Arctifat 6');
 
 //floor3 markers
-var floor3Marker1 = L.marker([-50, 30], { icon: markerIcon }).bindPopup('0,0'),
-    floor3Marker2 = L.marker([-40, 170], { icon: markerIcon }).bindPopup('0,0');
+var floor3Marker1 = L.marker([-50, 30], { icon: markerIcon }).bindPopup('Arctifat 7'),
+    floor3Marker2 = L.marker([-40, 170], { icon: markerIcon }).bindPopup('Arctifat 8');
 
 //floor4 markers
-var floor4Marker1 = L.marker([-80, 50], { icon: markerIcon }).bindPopup('0,0'),
-    floor4Marker2 = L.marker([-30, 90], { icon: markerIcon }).bindPopup('0,0');
+var floor4Marker1 = L.marker([-80, 50], { icon: markerIcon }).bindPopup('Arctifat 9'),
+    floor4Marker2 = L.marker([-30, 90], { icon: markerIcon }).bindPopup('Arctifat 10');
 
 //floor5 markers
 
-var floor5Marker1 = L.marker([-46, 94], { icon: markerIcon }).bindPopup('0,0'),
-    floor5Marker2 = L.marker([-60, 10], { icon: markerIcon }).bindPopup('0,0');
+var floor5Marker1 = L.marker([-46, 94], { icon: markerIcon }).bindPopup('Arctifat 11'),
+    floor5Marker2 = L.marker([-60, 10], { icon: markerIcon }).bindPopup('Arctifat 12');
 
 
 
