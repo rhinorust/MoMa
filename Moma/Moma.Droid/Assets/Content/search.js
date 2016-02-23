5a61f00a2aa162a71e88298fcd6b734f6c2b505e
jonathanhamel4 <jonathanhamel4@gmail.com>
2016-02-22 19:59:01 -0500

    Added jQuery Mobile to index.html

--- Moma/Moma.Droid/Assets/Content/search.js
+++ Moma/Moma.Droid/Assets/Content/search.js
@@ -1,18 +1,6 @@
 ﻿function showSearchBar() {
     var searchBarContainer = document.getElementById('searchBarDiv');
-    if (searchBarContainer.childNodes.length === 0) {
-        createSearchBar(searchBarContainer);
-    }
-    showHideSearch();
-}
-
-function createSearchBar(containerElement) {
-    var searchInput = document.createElement('input');
-}
-
-function showHideSearch() {
-    var searchBar = document.getElementById('searchBar');
-    if (searchBar != null) {
-        searchBar.style.display = searchBar.style.display === 'none' ? '' : 'none';
+    if (searchBarContainer != null) {
+        searchBarContainer.style.display = searchBarContainer.style.display === 'none' ? '' : 'none';
     }
 }
