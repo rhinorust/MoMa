df024c00e39bd09eae7862978274c36bfa2b0048
jonathanhamel4 <jonathanhamel4@gmail.com>
2016-02-23 20:44:20 -0500

    #JonathanHamel 'Dynamic search bar based on the js file. Onclick, complete bar and set coordinates. Missing to move to specified coordinates. Added POI into final_schema.js

--- Moma/Moma.Droid/Assets/Content/final_schema.js
+++ Moma/Moma.Droid/Assets/Content/final_schema.js
@@ -1,189 +1,362 @@
 var DATA = {
-  "floorPlan": [{
-    "floorID": "1",
-    "imagePath": "floor1/floor1.png",
-    "imageWidth": "7995",
-    "imageHeight": "3945"
-  },
-        {
-          "floorID": "2",
-          "imagePath": "floor2/floor2.png",
-          "imageWidth": "7995",
-          "imageHeight": "3945"
-        }, {
-          "floorID": "3",
-          "imagePath": "floor3/floor3.png",
-          "imageWidth": "7995",
-          "imageHeight": "3945"
-        },
-        {
-          "floorID": "4",
-          "imagePath": "floor4/floor4.png",
-          "imageWidth": "7995",
-          "imageHeight": "3945"
-        }, {
-          "floorID": "5",
-          "imagePath": "floor5/floor5.png",
-          "imageWidth": "7995",
-          "imageHeight": "3945"
-        }
-  ],
-
-  "node": [{
-    "poi": [{
-      "id": "P1",
-      "title": [{
-        "language": "English",
-        "title": "Artifact 1"
-      },
-				{
-				  "language": "French",
-				  "title": "Artéfact 1"
-				}
-      ],
-      "description": [{
-        "language": "English",
-        "description": "First artifact of the museum"
-      },
-    {
-      "language": "French",
-      "description": "Premier artéfact du musée"
+    "floorPlan": [{
+        "floorID": "1",
+        "imagePath": "floor1/floor1.png",
+        "imageWidth": "7995",
+        "imageHeight": "3945"
     },
-      ],
-      "x": "1245",
-      "y": "1024",
-      "floorID": "1",
-      "iBeacon": {
-        "uuid": "<UUID>",
-        "major": "<major>",
-        "minor": "<minor>"
-      },
-      "media": {
-        "image": [{
-          "path": "<URL>",
-          "language": "<language>",
-          "caption": "<caption>"
+          {
+              "floorID": "2",
+              "imagePath": "floor2/floor2.png",
+              "imageWidth": "7995",
+              "imageHeight": "3945"
+          }, {
+              "floorID": "3",
+              "imagePath": "floor3/floor3.png",
+              "imageWidth": "7995",
+              "imageHeight": "3945"
+          },
+          {
+              "floorID": "4",
+              "imagePath": "floor4/floor4.png",
+              "imageWidth": "7995",
+              "imageHeight": "3945"
+          }, {
+              "floorID": "5",
+              "imagePath": "floor5/floor5.png",
+              "imageWidth": "7995",
+              "imageHeight": "3945"
+          }
+    ],
+
+    "node": [{
+        "poi": [{
+            "id": "P1",
+            "title": [{
+                "language": "English",
+                "title": "Artifact 1"
+            },
+                      {
+                          "language": "French",
+                          "title": "Artéfact 1"
+                      }
+            ],
+            "description": [{
+                "language": "English",
+                "description": "First artifact of the museum"
+            },
+          {
+              "language": "French",
+              "description": "Premier artéfact du musée"
+          }
+            ],
+            "x": "1245",
+            "y": "1024",
+            "floorID": "1",
+            "iBeacon": {
+                "uuid": "<UUID>",
+                "major": "<major>",
+                "minor": "<minor>"
+            },
+            "media": {
+                "image": [{
+                    "path": "<URL>",
+                    "language": "<language>",
+                    "caption": "<caption>"
+                }
+                ],
+                "video": [{
+                    "path": "<URL>",
+                    "language": "<language>",
+                    "caption": "<caption>"
+                }
+                ],
+                "audio": [{
+                    "path": "<URL>",
+                    "language": "<language>",
+                    "caption": "<caption>"
+                }
+                ]
+            },
+            "storyPoint": [{
+                "storylineID": "S1",
+                "title": [{
+                    "language": "English",
+                    "title": "First Storyline"
+                },
+                            {
+                                "language": "French",
+                                "title": "Second storyline"
+                            }
+                ],
+                "description": [{
+                    "language": "English",
+                    "description": "First storyline of the museum"
+                }
+                ],
+                "media": {
+                    "image": [{
+                        "path": "<URL>",
+                        "language": "<language>",
+                        "caption": "<caption>"
+                    }
+                    ],
+                    "video": [{
+                        "path": "<URL>",
+                        "language": "<language>",
+                        "caption": "<caption>"
+                    }
+                    ],
+                    "audio": [{
+                        "path": "<URL>",
+                        "language": "<language>",
+                        "caption": "<caption>"
+                    }
+                    ]
+                }
+            }
+            ]
+        }, {
+            "id": "P2",
+            "title": [{
+                "language": "English",
+                "title": "Artifact 2"
+            },
+                      {
+                          "language": "French",
+                          "title": "Artéfact 2"
+                      }
+            ],
+            "description": [{
+                "language": "English",
+                "description": "First artifact of the museum"
+            },
+          {
+              "language": "French",
+              "description": "Premier artéfact du musée"
+          }
+            ],
+            "x": "1245",
+            "y": "1024",
+            "floorID": "1",
+            "iBeacon": {
+                "uuid": "<UUID>",
+                "major": "<major>",
+                "minor": "<minor>"
+            },
+            "media": {
+                "image": [{
+                    "path": "<URL>",
+                    "language": "<language>",
+                    "caption": "<caption>"
+                }
+                ],
+                "video": [{
+                    "path": "<URL>",
+                    "language": "<language>",
+                    "caption": "<caption>"
+                }
+                ],
+                "audio": [{
+                    "path": "<URL>",
+                    "language": "<language>",
+                    "caption": "<caption>"
+                }
+                ]
+            },
+            "storyPoint": [{
+                "storylineID": "S1",
+                "title": [{
+                    "language": "English",
+                    "title": "First Storyline"
+                },
+                            {
+                                "language": "French",
+                                "title": "Second storyline"
+                            }
+                ],
+                "description": [{
+                    "language": "English",
+                    "description": "First storyline of the museum"
+                }
+                ],
+                "media": {
+                    "image": [{
+                        "path": "<URL>",
+                        "language": "<language>",
+                        "caption": "<caption>"
+                    }
+                    ],
+                    "video": [{
+                        "path": "<URL>",
+                        "language": "<language>",
+                        "caption": "<caption>"
+                    }
+                    ],
+                    "audio": [{
+                        "path": "<URL>",
+                        "language": "<language>",
+                        "caption": "<caption>"
+                    }
+                    ]
+                }
+            }
+            ]
         },
+        {
+            "id": "P3",
+            "title": [{
+                "language": "English",
+                "title": "Artifact 3"
+            },
+                      {
+                          "language": "French",
+                          "title": "Artéfact 3"
+                      }
+            ],
+            "description": [{
+                "language": "English",
+                "description": "First artifact of the museum"
+            },
+          {
+              "language": "French",
+              "description": "Premier artéfact du musée"
+          }
+            ],
+            "x": "1245",
+            "y": "1024",
+            "floorID": "1",
+            "iBeacon": {
+                "uuid": "<UUID>",
+                "major": "<major>",
+                "minor": "<minor>"
+            },
+            "media": {
+                "image": [{
+                    "path": "<URL>",
+                    "language": "<language>",
+                    "caption": "<caption>"
+                }
+                ],
+                "video": [{
+                    "path": "<URL>",
+                    "language": "<language>",
+                    "caption": "<caption>"
+                }
+                ],
+                "audio": [{
+                    "path": "<URL>",
+                    "language": "<language>",
+                    "caption": "<caption>"
+                }
+                ]
+            },
+            "storyPoint": [{
+                "storylineID": "S1",
+                "title": [{
+                    "language": "English",
+                    "title": "First Storyline"
+                },
+                            {
+                                "language": "French",
+                                "title": "Second storyline"
+                            }
+                ],
+                "description": [{
+                    "language": "English",
+                    "description": "First storyline of the museum"
+                }
+                ],
+                "media": {
+                    "image": [{
+                        "path": "<URL>",
+                        "language": "<language>",
+                        "caption": "<caption>"
+                    }
+                    ],
+                    "video": [{
+                        "path": "<URL>",
+                        "language": "<language>",
+                        "caption": "<caption>"
+                    }
+                    ],
+                    "audio": [{
+                        "path": "<URL>",
+                        "language": "<language>",
+                        "caption": "<caption>"
+                    }
+                    ]
+                }
+            }
+            ]
+        }
         ],
-        "video": [{
-          "path": "<URL>",
-          "language": "<language>",
-          "caption": "<caption>"
-        },
-        ],
-        "audio": [{
-          "path": "<URL>",
-          "language": "<language>",
-          "caption": "<caption>"
-        },
+
+        "pot": [{
+            "id": "<ID number>",
+            "label": {
+                "language": "<language>",
+                "label": "<ramp or stairs or elevator or intersection or washroom or exit or entrance or emergency exit>"
+            },
+            "x": "<coordinate x>",
+            "y": "<coordinate y>",
+            "floorID": "<floor ID>",
+        }, {
+            "id": "<ID number>",
+            "label": {
+                "language": "<language>",
+                "label": "<ramp or stairs or elevator or intersection or washroom or exit or entrance or emergency exit>"
+            },
+            "x": "<coordinate x>",
+            "y": "<coordinate y>",
+            "floorID": "<floor ID>",
+        }
         ]
-      },
-      "storyPoint": [{
-        "storylineID": "S1",
+    }
+    ],
+
+    "edge": [{
+        "startNode": "<node>",
+        "endNode": "<node>",
+        "floorNumber": "<floor ID>",
+        "distance": "<distance>"
+    }
+    ],
+
+    "storyline": [{
+        "id": "<ID number>",
         "title": [{
-          "language": "English",
-          "title": "First Storyline"
-        },
-					{
-					  "language": "French",
-					  "title": "Second storyline"
-					},
+            "language": "<language>",
+            "title": "Storyline 1"
+        }
         ],
         "description": [{
-          "language": "English",
-          "description": "First storyline of the museum"
-        },
-        ],
-        "media": {
-          "image": [{
-            "path": "<URL>",
             "language": "<language>",
-            "caption": "<caption>"
-          },
-          ],
-          "video": [{
-            "path": "<URL>",
-            "language": "<language>",
-            "caption": "<caption>"
-          },
-          ],
-          "audio": [{
-            "path": "<URL>",
-            "language": "<language>",
-            "caption": "<caption>"
-          },
-          ]
+            "description": "<description>"
         }
-      },
-      ],
+        ],
+        "path": ["<poiID or potID>", , "<poiID or potID>"],
+        "thumbnail": "<path of thumbnail>",
+        "walkingTimeInMinutes": "<minutes>",
+        "floorsCovered": "<number of floors covered>"
     },
-    ],
-
-    "pot": [{
-      "id": "<ID number>",
-      "label": {
-        "language": "<language>",
-        "label": "<ramp or stairs or elevator or intersection or washroom or exit or entrance or emergency exit>"
-      },
-      "x": "<coordinate x>",
-      "y": "<coordinate y>",
-      "floorID": "<floor ID>",
-    }, {
-      "id": "<ID number>",
-      "label": {
-        "language": "<language>",
-        "label": "<ramp or stairs or elevator or intersection or washroom or exit or entrance or emergency exit>"
-      },
-      "x": "<coordinate x>",
-      "y": "<coordinate y>",
-      "floorID": "<floor ID>",
+    {
+        "id": "<ID number>",
+        "title": [{
+            "language": "<language>",
+            "title": "Storyline 2"
+        }
+        ],
+        "description": [{
+            "language": "<language>",
+            "description": "<description>"
+        }
+        ],
+        "path": ["<poiID or potID>", , "<poiID or potID>"],
+        "thumbnail": "<path of thumbnail>",
+        "walkingTimeInMinutes": "<minutes>",
+        "floorsCovered": "<number of floors covered>"
     }
+
     ]
-  }
-  ],
-
-  "edge": [{
-    "startNode": "<node>",
-    "endNode": "<node>",
-    "floorNumber": "<floor ID>",
-    "distance": "<distance>"
-  },
-  ],
-
-  "storyline": [{
-    "id": "<ID number>",
-    "title": [{
-      "language": "<language>",
-      "title": "Storyline 1"
-    },
-    ],
-    "description": [{
-      "language": "<language>",
-      "description": "<description>"
-    },
-    ],
-    "path": ["<poiID or potID>", , "<poiID or potID>"],
-    "thumbnail": "<path of thumbnail>",
-    "walkingTimeInMinutes": "<minutes>",
-    "floorsCovered": "<number of floors covered>"
-  },
-  {
-      "id": "<ID number>",
-      "title": [{
-          "language": "<language>",
-          "title": "Storyline 2"
-      },
-      ],
-      "description": [{
-          "language": "<language>",
-          "description": "<description>"
-      },
-      ],
-      "path": ["<poiID or potID>", , "<poiID or potID>"],
-      "thumbnail": "<path of thumbnail>",
-      "walkingTimeInMinutes": "<minutes>",
-      "floorsCovered": "<number of floors covered>"
-  },
-
-  ]
 };
--- Moma/Moma.Droid/Assets/Content/search.js
+++ Moma/Moma.Droid/Assets/Content/search.js
@@ -1,1 +1,47 @@
-﻿
+﻿//creates the list view dynamically based on the js file
+function createListView() {
+    var language = getLanguage();
+    var ul = $('#listViewUl');
+    var poi = DATA.node[0].poi;
+    for (var i = 0; i < poi.length; i++) {
+        var li = document.createElement('li');
+        var aTag = document.createElement('a');
+        //aTag.href = "#";
+        var foundTitle = false;
+        for (var j = 0; j < poi[i].title.length; j++) {
+            if (poi[i].title[j].language == language) {
+                aTag.innerHTML = poi[i].title[j].title;
+                aTag.id = poi[i].id;
+                aTag.addEventListener("click", focusOnNode, false);
+                li.appendChild(aTag);
+                foundTitle = true;
+                break;
+            }
+        }
+        if (foundTitle) {
+            ul.append(li);
+            ul.listview('refresh');
+        }
+    }
+}
+
+//Should get the language from the resource file
+function getLanguage() {
+    return "English";
+}
+
+function focusOnNode(node) {
+    node.preventDefault();
+    $('li').attr("class", "ui-screen-hidden");
+    var id = node.target.id;
+    $('#autocomplete-input').val(node.target.innerHTML);
+    var poi = DATA.node[0].poi;
+    var coordinates = {};
+    for (var i = 0; i < poi.length; i++) {
+        if(poi[i].id == id) {
+            coordinates.x = poi[i].x;
+            coordinates.y = poi[i].y;
+        }
+    }
+    //map.panTo(new L.LatLngBounds(x,y));
+}
