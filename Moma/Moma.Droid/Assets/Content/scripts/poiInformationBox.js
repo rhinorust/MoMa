a1407f4aed22c46a987e0b408de8ed876c7dcbab
Helgi Tomas Gislason <helgso@gmail.com>
2016-03-14 19:06:30 -0400

    XM-19 #CodeReview 1. Added support for audio and video inside the POI information box.
    2. Included audio and video samples Nikolaos posted on Moodle from Anja.
    
    3. Included an example JSON.txt document that is loaded and injected into javascript during startup and accessed by the javascript function iBeaconDiscovered() so that it knows what iBeacon is linked to what image/audio/video/text.
    
    4. Figured out how to call C# from Javascript, see functions in JSBridge.cs for examples. After creating your own in there, you then call jsBridge.yourFunction() in javascript to call the C# yourFunction() inside JSBridge.cs

--- Moma/Moma.Droid/Assets/Content/scripts/poiInformationBox.js
+++ Moma/Moma.Droid/Assets/Content/scripts/poiInformationBox.js
@@ -1,31 +1,92 @@
 var poiIB;
+var data; // JSON.txt data will be stored here by C#
+// Temporary, to be fixed later
+var audioFileName  = "";
 
-$('document').ready(function() {
-	poiIB = $('#POI_InformationBox');
+$('document').ready(function () {
+    poiIB = $('#POI_InformationBox');
 
-	$('#closeButton').click(function() {
-		poiIB.css('visibility', 'hidden');
-	})
+    $('#close').click(function () {
+        poiIB.css('visibility', 'hidden');
+        stopAudioAndVideo()
+    });
+
+    // Debugging: For opening the POI information box without an iBeacon
+	//iBeaconDiscovered(9377, 54177);
 });
 
 function iBeaconDiscovered(major, minor) {
-	var title    = poiIB.find('#title h1');
-	var content  = poiIB.find('#content');
+    var title = poiIB.find('#title h1');
+    var content = poiIB.find('#content');
 
-	var titleText = "";
-	var contentImg = "";
-	var contentText = "";
+    var boxTitle = "";
+    var boxContent = "";
 
-	// CocaCola iBeacon
-	if (major === 9377 && minor === 54177) {
-		titleText = "COCACOLA IBEACON FOUND";
-		contentText = "<p>Major: "+major+", Minor: "+minor+"</p>";
-		contentImg = '<img src="images/CocaColaBeacon.png">';
-	}
+    var pois = data.node.poi
+    // Find the POI with the given major and minor and
+    // populate the POI information box with their content
+    // Finally, display the POI information box.
+    for (var i = 0; i < pois.length; i++) {
+        var poi = pois[i];
+        var iBeacon = poi.iBeacon;
+        if (iBeacon.major === major && iBeacon.minor === minor) {
+            var poiDescription = poi.description[0].description;
+            var poiTitle = poi.title[0].title;
+            // Add the media elements to the POI information box
+            var images = poi.media.image;
+            var videos = poi.media.video;
+            var audio  = poi.media.audio;
+            // If there are images
+            if (images.length > 0) {
+                for (var j = 0; j < images.length; j++)
+                    boxContent += imagef(images[j].path);
+            }   
+            // If there are videos
+            if (videos.length > 0) {
+                // Only add one for now
+                boxContent += videof(videos[0].path);
+            }
+            // If there is audio
+            if (audio.length > 0) {
+                // Only add one for now
+                audioFileName = audio[0].path;
+                boxContent += audiof(audio[0].path);
+            }
 
-	title.text(titleText);
-	content.empty();
-	content.append(contentText + contentImg);
+            boxContent += textf(poiDescription);
+            boxTitle = poiTitle;
 
-	poiIB.css('visibility', 'visible');
+            title.text(boxTitle);
+            content.empty();
+            content.append(boxContent);
+
+            poiIB.css('visibility', 'visible');
+
+            break;
+        }
+    }
+}
+
+function videof(fileName) {
+    return '<video controls="controls" src="'+fileName+'"></video>';
+}
+
+function imagef(fileName) {
+    return '<img src="'+fileName+'">';
+}
+
+function audiof(fileName) {
+    return '<img src="images/playAudio.png" onClick="jsBridge.playOrStopAudioFile(\''+fileName+'\')">';
+}
+
+function textf(fileName) {
+    return '<p>'+fileName+'</p>';
+}
+
+function stopAudioAndVideo() {
+    var vid = $('video');
+    if (vid) {
+        vid.get(0).pause();
+    }
+    jsBridge.stopAudioFile(audioFileName);
 }
