ccefab590daa196a7885f5c7e1dc10bb38dde2e1
jonathanhamel4 <jonathanhamel4@gmail.com>
2016-03-20 21:22:13 -0400

    Added path in data_local to fix storyline2. #JonathanHamel

--- Moma/Moma.Droid/Assets/Content/data/data_local.js
+++ Moma/Moma.Droid/Assets/Content/data/data_local.js
@@ -1048,8 +1048,9 @@ var DATA = {
             }
           ],
           "path": [
-            "<poiID or potID>",
-            "<poiID or potID>"
+            "0000",
+            "1000",
+            "1001"
           ],
           "thumbnail": "<path of thumbnail>",
           "walkingTimeInMinutes": "<minutes>",
