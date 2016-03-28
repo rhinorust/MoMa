7bf9a2c3dbd3919a4c8c814c4c606c85ca341bd5
Roberto Ruffolo <robertoruffolo@hotmail.com>
2016-03-28 18:42:41 -0400

    XM-73 #CodeReview Removed console logs and no longer supported functionality. #RobertoRuffoloBenavides

--- Moma/Moma.Droid/Assets/Content/scripts/storyline_init.js
+++ Moma/Moma.Droid/Assets/Content/scripts/storyline_init.js
@@ -1,19 +1,8 @@
 ﻿function init() {
-    if (localStorage.getItem("startIsSelected") == "true" && localStorage.getItem("currentStoryline") != null) {
-        console.log("****************** " + localStorage.getItem("currentStoryline") + " *************");
-        startStoryline(localStorage.getItem("currentStoryline"));
-    
-    }
-    else if (localStorage.getItem("currentStoryline") != null) {
-        console.log("****************** " + localStorage.getItem("currentStoryline") + " *************");
-        previewStoryline(localStorage.getItem("currentStoryline"));
-    }
-    else {
-        for (var i = 1, l = DATA.storyline.length; i <= l; i++) {
-            var storylineID = DATA.storyline[i - 1].id + "";
-            var content = "<div data-role='collapsible' id='storyline" + i + "'><h3>" + DATA.storyline[(i - 1)].title[0].title + "</h3><p>" + DATA.storyline[(i - 1)].description[0].description + "</p><br><input type='submit' value='Start' onclick='startStoryline(\"" + storylineID + "\")' style='background-color:rgba(61,139,255,1); color:white; border-radius:2px; border-color:rgba(61,139,255,1);'/>&nbsp;&nbsp;<input type='submit' value='Preview' onclick='previewStoryline(\"" + storylineID + "\")' style='background-color:rgba(61,139,255,1); color:white; border-radius:2px; border-color:rgba(61,139,255,1);'/></div>";
-            $("#storylines").append(content).collapsibleset('refresh');
-        }
+    for (var i = 1, l = DATA.storyline.length; i <= l; i++) {
+        var storylineID = DATA.storyline[i - 1].id + "";
+        var content = "<div data-role='collapsible' id='storyline" + i + "'><h3>" + DATA.storyline[(i - 1)].title[0].title + "</h3><p>" + DATA.storyline[(i - 1)].description[0].description + "</p><br><input type='submit' value='Start' onclick='startStoryline(\"" + storylineID + "\")' style='background-color:rgba(61,139,255,1); color:white; border-radius:2px; border-color:rgba(61,139,255,1);'/>&nbsp;&nbsp;<input type='submit' value='Preview' onclick='previewStoryline(\"" + storylineID + "\")' style='background-color:rgba(61,139,255,1); color:white; border-radius:2px; border-color:rgba(61,139,255,1);'/></div>";
+        $("#storylines").append(content).collapsibleset('refresh');
     }
 }
 
