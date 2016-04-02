3c298b074a808f56fc38153936b435e8d64fe817
Roberto Ruffolo <robertoruffolo@hotmail.com>
2016-04-02 12:02:49 -0400

    XM-73 HotFix, accidentally removed logic to fix an edge case. Reinserting code now. #RobertoRuffoloBenavides

--- Moma/Moma.Droid/Assets/Content/scripts/storyline_init.js
+++ Moma/Moma.Droid/Assets/Content/scripts/storyline_init.js
@@ -1,8 +1,17 @@
 ﻿function init() {
-    for (var i = 1, l = DATA.storyline.length; i <= l; i++) {
-        var storylineID = DATA.storyline[i - 1].id + "";
-        var content = "<div data-role='collapsible' id='storyline" + i + "'><h3>" + DATA.storyline[(i - 1)].title[0].title + "</h3><p>" + DATA.storyline[(i - 1)].description[0].description + "</p><br><input type='submit' value='Start' onclick='startStoryline(\"" + storylineID + "\")' style='background-color:rgba(61,139,255,1); color:white; border-radius:2px; border-color:rgba(61,139,255,1);'/>&nbsp;&nbsp;<input type='submit' value='Preview' onclick='previewStoryline(\"" + storylineID + "\")' style='background-color:rgba(61,139,255,1); color:white; border-radius:2px; border-color:rgba(61,139,255,1);'/></div>";
-        $("#storylines").append(content).collapsibleset('refresh');
+    if (localStorage.getItem("startIsSelected") == "true" && localStorage.getItem("currentStoryline") != null) {
+        startStoryline(localStorage.getItem("currentStoryline"));
+
+    }
+    else if (localStorage.getItem("currentStoryline") != null) {
+        previewStoryline(localStorage.getItem("currentStoryline"));
+    }
+    else {
+        for (var i = 1, l = DATA.storyline.length; i <= l; i++) {
+            var storylineID = DATA.storyline[i - 1].id + "";
+            var content = "<div data-role='collapsible' id='storyline" + i + "'><h3>" + DATA.storyline[(i - 1)].title[0].title + "</h3><p>" + DATA.storyline[(i - 1)].description[0].description + "</p><br><input type='submit' value='Start' onclick='startStoryline(\"" + storylineID + "\")' style='background-color:rgba(61,139,255,1); color:white; border-radius:2px; border-color:rgba(61,139,255,1);'/>&nbsp;&nbsp;<input type='submit' value='Preview' onclick='previewStoryline(\"" + storylineID + "\")' style='background-color:rgba(61,139,255,1); color:white; border-radius:2px; border-color:rgba(61,139,255,1);'/></div>";
+            $("#storylines").append(content).collapsibleset('refresh');
+        }
     }
 }
 
