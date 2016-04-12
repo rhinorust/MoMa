ba76bfc39b2adebf6d0ade963cdfdded6ac44b33
avinanda <avinanda.chatt@live.com>
2016-04-12 04:43:37 -0400

    XM-32 Added appropriate links to some info to directly open the selected items link.

--- Moma/Moma/ContactPage.xaml.cs
+++ Moma/Moma/ContactPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.ServiceModel.Channels;
 using System.Text;
 using System.Threading.Tasks;
@@ -33,20 +34,64 @@ namespace Moma
         public async void OnPhoneTapped(object sender, EventArgs e)
         {
             if (await this.DisplayAlert(
-                             "Dial a Number",
-                             "Call 514 - 932 - 9663 ?",
-                             "Yes",
-                             "No"))
+                "Exit",
+                "Are you sure you want to leave the application to call?",
+                "Yes",
+                "No"))
             {
                 var dialer = DependencyService.Get<IDialer>();
                 if (dialer != null)
                 {
-                    dialer.Dial("514 - 932 - 9663");
+                    dialer.Dial("514-932-9663");
                 }
             }
         }
 
+        public async void OnWebsiteTapped(object sender, EventArgs e)
+        {
+            if (await this.DisplayAlert(
+                "Exit",
+                "Are you sure you want to leave the application to open the website?",
+                "Yes",
+                "No"))
+            {
+                var url = DependencyService.Get<IWebPageOpener>();
+                if (url != null)
+                {
+                    url.OpenBrowser("http://moeb.ca/en");
+                }
+            }
+        }
+
+        public async void DirectionTapped(object sender, EventArgs e)
+        {
+            var networkConnection = DependencyService.Get<INetworkConnection>();
+            networkConnection.CheckNetworkConnection();
+            var networkStatus = networkConnection.IsConnected;
+
+
+            if (networkStatus)
+            {
+                var answer = await DisplayAlert("Exit", "Are you sure you want to leave the application?", "Yes", "No");
+                if (answer == true)
+                {
+                    Uri uri =
+                        new Uri(
+                            "https://www.google.ca/maps/place/1001+Rue+Lenoir,+Montr%C3%A9al,+QC+H4C+2Z6/@45.4766472,-73.5924109,17z/data=!4m2!3m1!1s0x4cc9109f3e1c689d:0xfe5e29cc968bfa44");
+                    Device.OpenUri(uri);
+                }
+                else
+                {
+                    await Navigation.PushAsync(new DirectionsPage());
+                }
+            }
+            else
+            {
+                await Navigation.PushAsync(new DirectionsPage());
+            }
+
+        }
+
+
     }
-
-
 }
