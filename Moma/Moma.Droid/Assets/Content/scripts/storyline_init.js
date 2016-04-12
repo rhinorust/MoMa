1ab0b2de5089ef48d91e2277857f58f9a5bc1d9b
Roberto Ruffolo <robertoruffolo@hotmail.com>
2016-04-12 12:20:14 -0400

    XM-85 Resume button only displays for started storyline. Actove storyline popup still has few issues that need to be addressed #Roberto

--- Moma/Moma.Droid/Assets/Content/scripts/storyline_init.js
+++ Moma/Moma.Droid/Assets/Content/scripts/storyline_init.js
@@ -1,11 +1,11 @@
 function init() {
     for (var i = 1, l = DATA.storyline.length; i <= l; i++) {
         var storylineID = DATA.storyline[i - 1].id + "";
-        var content = "<div data-role='collapsible' id='storyline" + i + "'><h3>" + DATA.storyline[(i - 1)].title + "</h3><p>" + DATA.storyline[(i - 1)].description + "</p><br><input type='submit' value='Start' onclick='startStoryline(\"" + storylineID + "\")' style='background-color:rgba(61,139,255,1); color:white; border-radius:2px; border-color:rgba(61,139,255,1);'/>&nbsp;&nbsp;<input type='submit' value='Preview' onclick='previewStoryline(\"" + storylineID + "\")' style='background-color:rgba(61,139,255,1); color:white; border-radius:2px; border-color:rgba(61,139,255,1);<input type='submit' id = 'resume\"" + storylineID + "\"' value='Resume' onclick='resumeStoryline(\"" + storylineID + "\")' style='background-color:rgba(61,139,255,1); color:white; border-radius:2px; border-color:rgba(61,139,255,1); visibility:hidden;'/>&nbsp;&nbsp;'/></div>";
+        var content = "<div data-role='collapsible' id='storyline" + i + "'><h3>" + DATA.storyline[(i - 1)].title[0].title + "</h3><p>" + DATA.storyline[(i - 1)].description[0].description + "</p><br><input type='submit' value='Start' onclick='startStoryline(\"" + storylineID + "\")' style='background-color:rgba(61,139,255,1); color:white; border-radius:2px; border-color:rgba(61,139,255,1);'/>&nbsp;&nbsp;<input type='submit' value='Preview' onclick='previewStoryline(\"" + storylineID + "\")' style='background-color:rgba(61,139,255,1); color:white; border-radius:2px; border-color:rgba(61,139,255,1);'/>&nbsp;&nbsp;<input type='submit' id = 'resume" + storylineID + "' value='Resume' onclick='resumeStoryline(" + storylineID + ")' style='background-color:rgba(61,139,255,1); color:white; border-radius:2px; border-color:rgba(61,139,255,1); visibility:hidden;&nbsp;&nbsp;'/></div>";
         $("#storylines").append(content).collapsibleset('refresh');
     }
     if (localStorage.getItem("currentStoryline") != null) {
-        var hiddenResume = $("#storyline" + localStorage.getItem("currentStoryline") + ":nth-child(2)");
+        var hiddenResume = $("#resume" + localStorage.getItem("currentStoryline"));
         hiddenResume.css("visibility","visible");
     }
 }
@@ -16,11 +16,13 @@ function previewStoryline(storylineID) {
 }
 
 function startStoryline(storylineID) {
-    if (localStorage.getItem("currentStoryline") != storylineID)
+    if (localStorage.getItem("currentStoryline") != storylineID && localStorage.getItem("currentStoryline") != null)
         jsBridge.confirmPopup(storylineID);
-   // localStorage.setItem("currentStoryline", storylineID);
-    //localStorage.setItem("startIsSelected", "true");
-   // window.location.replace("storyline_index.html");
+    else {
+        localStorage.setItem("currentStoryline", storylineID);
+        localStorage.setItem("startIsSelected", "true");
+        window.location.replace("storyline_index.html");
+    }
 }
 
 function resumeStoryline(storylineID) {
